337c39dab40359f22f7c9c656ce1e0e06d6772fc
Pasta-amongus <88669889+Pasta-amongus@users.noreply.github.com>
2021-09-05 12:23:08 +0000

    Create message.py

---
+++ message.py
@@ -0,0 +1,9 @@
+import os,fortnitepy,datetime,requests,json,asyncio,time,random,threading
+
+from threading import Thread
+
+from Fortnite import Variants,API,Extras,colored,apiwrapper
+
+fnapi = apiwrapper.FortniteAPI()
+
+async def Command(self, message)
